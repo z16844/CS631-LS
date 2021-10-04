0bc2ee66a61cb80c9168c27b4ddf462700553fc5
Tachyon <z16844@icloud.com>
2021-10-04 14:23:32 -0400

    WIP: lexicographical order

--- sorting.c
+++ sorting.c
@@ -1,4 +1,27 @@
+#include <stdbool.h>
+
+#include "misc.h"
 #include "sorting.h"
+
+void
+order_lexicographical(PENTRY root)
+{
+	PENTRY t = NULL;
+	PENTRY cur = root;
+	bool hasChanged = true;
+	while (hasChanged) {
+		hasChanged = false;
+		while (cur->next != NULL) {
+			if (strcmp(cur->filename, cur->next->filename) > 0) {
+				hasChanged = true;
+				t = cur
+			}
+		}
+		// Still working on it
+		cur = cur->next;
+	}
+	return cursor;
+}
 
 PENTRY
 order_reversal(PENTRY root)
@@ -18,9 +41,11 @@ order_reversal(PENTRY root)
 	}
 	return root;
 }
+
 PENTRY
 sort(PENTRY root, const POPTIONS options)
 {
+	// order_lexicographical(root);
 	if (options->ReverseTheLexicographicalOrder)
 		return order_reversal(root);
 	return root;

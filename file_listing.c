bfd3afa05278d383baaf0ab337e242dea8e2ada6
Tachyon <z16844@icloud.com>
2021-10-03 23:04:31 -0400

    Implemented -a and -A options

--- file_listing.c
+++ file_listing.c
@@ -1,12 +1,71 @@
+#include <sys/param.h>
+
 #include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "file_listing.h"
 #include "parameters.h"
 
-void
+char filetype_symbol[] = "aAbcdlspw-";
+
+PENTRY
+convert(struct dirent *entry, char *location)
+{
+	PENTRY result = (PENTRY)calloc(1, sizeof(ENTRY));
+	bzero(result, sizeof(ENTRY));
+
+	int read_length = MAX(PATH_MAX, strlen(entry->d_name));
+	strncpy(result->filename, entry->d_name, read_length);
+
+	location = NULL;
+	location = location;
+	// char *file_path =
+	//     strncat(location, result->filename, strlen(result->filename));
+	// struct stat f_stat = { 0 };
+	// if (stat(file_path, &f_stat) != EXIT_SUCCESS) {
+	// 	fprintf(stderr, "Failed to read stat: %s\n", file_path);
+	// 	exit(EXIT_FAILURE);
+	// }
+
+	switch (entry->d_type) {
+	case DT_BLK:
+		result->type = Block_special_file;
+		break;
+	case DT_CHR:
+		result->type = Character_special_file;
+		break;
+	case DT_DIR:
+		result->type = Directory;
+		break;
+	case DT_FIFO:
+		result->type = FIFO;
+		break;
+	case DT_SOCK:
+		result->type = Socket_link;
+		break;
+	case DT_WHT:
+		result->type = Whiteout;
+		break;
+	case DT_LNK:
+		result->type = Symbolic_link;
+		break;
+	case DT_REG:
+	default:
+		result->type = Regular_file;
+		break;
+	}
+
+	return result;
+}
+PENTRY
 travel_directory(const POPTIONS options)
 {
+
+	PENTRY query_set = (PENTRY)calloc(1, sizeof(ENTRY));
+	bzero(query_set, sizeof(ENTRY));
+
 	DIR *d;
 	struct dirent *dir;
 	int index = options->CountPaths;
@@ -15,9 +74,26 @@ travel_directory(const POPTIONS options)
 		d = opendir(path);
 		if (d != NULL) {
 			while ((dir = readdir(d)) != NULL) {
-				printf("%s\t\t", dir->d_name);
+				if (is_visible(options, dir))
+					printf("%s\t", dir->d_name);
 			}
 			closedir(d);
 		}
 	}
+	return query_set;
+}
+bool
+is_visible(const POPTIONS options, struct dirent *entry)
+{
+	if (options->ListAllEntries && entry->d_type == DT_DIR) {
+		if (strcmp(".", entry->d_name) == 0 ||
+		    strcmp("..", entry->d_name) == 0) {
+			return false;
+		}
+	}
+	if (!options->IncludeDirectoryEntries) {
+		if (entry->d_name[0] == '.')
+			return false;
+	}
+	return true;
 }
--- file_listing.h
+++ file_listing.h
@@ -1,20 +1,40 @@
 #include <sys/stat.h>
 
+#include <dirent.h>
 #include <limits.h>
+#include <stdbool.h>
 
 #include "parameters.h"
 
 #ifndef _LISTING_H_
 #define _LISTING_H_
 
+enum filetype
+{
+	Archive_state_1,	   // a
+	Archive_state_2,	   // A
+	Block_special_file,	   // b
+	Character_special_file,	   // c
+	Directory,		   // d
+	Symbolic_link,		   // l
+	Socket_link,		   // s
+	FIFO,			   // p
+	Whiteout,		   // w
+	Regular_file,		   // −
+};
+
 typedef struct ENTRY
 {
 	struct stat info;
-	char path[PATH_MAX];
-	struct ENTRY *subentries;
+	char filename[PATH_MAX];
+	enum filetype type;
+	struct ENTRY **children;
 } ENTRY, *PENTRY;
 
-void
+PENTRY
 travel_directory(const POPTIONS options);
+
+bool
+is_visible(const POPTIONS options, struct dirent *entry);
 
 #endif /* !_LISTING_H_ */

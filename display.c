71a1a3d22a74d89c5c2ab73c1e6a3bd95c21e91f
Tachyon <z16844@icloud.com>
2021-10-17 03:24:53 -0400

    Refactored print_long_format

--- display.c
+++ display.c
@@ -23,85 +23,81 @@ char *filetype_symbols = "/@=|%%\0\0\0\0\0";
 void
 print_long_format(PENTRY entry, const POPTIONS options)
 {
-	int offset = -1;
-	int len_DateTime = 11;
-	int buf_size = 10			 /* filetype and permission*/
-		       + 1			 /* blank */
-		       + setting->maxHardLinks	 /* hardlink field */
-		       + 1			 /* blank */
-		       + setting->maxUserLen	 /* owner field */
-		       + 1			 /* blank */
-		       + setting->maxGroupLen	 /* group field */
-		       + 1			 /* blank */
-		       + setting->maxSizeLen	 /* file size field */
-		       + 1			 /* blank */
-		       + len_DateTime		 /* DateTime field */
-		       + 1			 /* blank before filename*/
-		       + strlen(entry->filename) /* Filename field */
-		       + 1			 /* NULL-Terminator */
-	    ;
-	char *line_buf = (char *)calloc_checked(buf_size, sizeof(char));
-	memset(line_buf, '\x20', buf_size);
-	line_buf[buf_size - 1] = '\x00';
+	/****sequence columns in -l with other options
+	 * inode (-i)
+	 * blocks (-s)
+	 * mode
+	 * # of hardlinks
+	 * owner name
+	 * group name
+	 * bytes of the file
+	 * last modified date/time
+	 * pathname
+	 */
+	int len_blank = 0;
+	if (options->ShowInodeNumber) {	   // -i
+		char *inode = itoa(entry->info.st_ino);
+		int lenInode = strlen(inode);
+		len_blank = setting->maxInodeLen - lenInode;
+		for (; len_blank > 0; len_blank--)
+			printf(" ");
+		printf("%s ", inode);
+		free(inode);
+		len_blank = 0;
+	}
+	if (options->DisplayByBlockSize) {    // -s
+		char *blocks = itoa(entry->info.st_blocks);
+		int len_blocks = strlen(blocks);
+
+		len_blank = setting->maxBlockSizeLen - len_blocks;
+		for (; len_blank > 0; len_blank--)
+			printf(" ");
+		printf("%s ", blocks);
+		free(blocks);
+		len_blank = 0;
+	}
 
 	char mode[11] = { 0 };
 	strmode(entry->info.st_mode, mode);
-	offset = strlen(mode);
-	strncpy(line_buf, mode, offset);
+	printf("%s ", mode);
 
 	char *hardlinks = itoa(entry->info.st_nlink);
-	offset += setting->maxHardLinks - strlen(hardlinks);
-	strncpy(&(line_buf[offset]), hardlinks, setting->maxHardLinks);
+	len_blank = setting->maxHardLinks - strlen(hardlinks);
+	for (; len_blank > 0; len_blank--)
+		printf(" ");
+	printf("%s ", hardlinks);
 	free(hardlinks);
 
 	char *username = options->ShowAsUidAndGid
 			     ? itoa(entry->info.st_uid)
 			     : getUserName(entry->info.st_uid);
-	offset +=
-	    strlen(hardlinks) + 1 + setting->maxUserLen - strlen(username);
-	strncpy(&(line_buf[offset]), username, strlen(username));
+	len_blank = setting->maxUserLen - strlen(username);
+	for (; len_blank > 0; len_blank--)
+		printf(" ");
+	printf("%s ", username);
 
 	char *group_name = options->ShowAsUidAndGid
 			       ? itoa(entry->info.st_gid)
 			       : getGroupName(entry->info.st_gid);
-	offset +=
-	    strlen(username) + 1 + setting->maxGroupLen - strlen(group_name);
-	strncpy(&(line_buf[offset]), group_name, strlen(group_name));
+	len_blank = setting->maxGroupLen - strlen(group_name);
+	for (; len_blank > 0; len_blank--)
+		printf(" ");
+	printf("%s ", group_name);
 
-	char *size;
-	if (options->HumanReadableFormat) {
-		size = asHumanReadable(entry->info.st_size);
-	} else
-		size = itoa(entry->info.st_size);
-	offset += strlen(group_name) + 1 + setting->maxSizeLen - strlen(size);
-	strncpy(&(line_buf[offset]), size, strlen(size));
+	char *size = options->HumanReadableFormat
+			 ? asHumanReadable(entry->info.st_size)
+			 : itoa(entry->info.st_size);
+	len_blank = setting->maxSizeLen - strlen(size);
+	for (; len_blank > 0; len_blank--)
+		printf(" ");
+	printf("%s ", size);
 	free(size);
 
 	char *timestamp = parse_timestamp(&entry->info.st_mtime);
-	offset += strlen(size) + 1;
-	strncpy(&(line_buf[offset]), timestamp, strlen(timestamp));
+	printf("%s ", timestamp);
 	free(timestamp);
 
-	offset += strlen(timestamp) + 1;
-	int lenFilename = strlen(entry->filename);
-	strncpy(&(line_buf[offset]), entry->filename, lenFilename);
-	offset += lenFilename;
-
-	if (options->ShowInodeNumber) {
-		char *inode = itoa(entry->info.st_ino);
-		int lenInode = strlen(inode);
-		char *inodeField = (char *)calloc_checked(
-		    setting->maxInodeLen + 1, sizeof(char));
-		offset += setting->maxInodeLen - lenInode;
-		strncpy(&inodeField[setting->maxInodeLen - lenInode], inode,
-			lenInode);
-		inodeField[lenInode] = '\x20';
-		line_buf = strncat(inodeField, line_buf, strlen(line_buf));
-		offset += lenInode + 1;
-		free(inodeField);
-	}
-	line_buf[offset] = '\x00';
-	printf(line_buf);
+	printf("%s ", entry->filename);
 	printf("\n");
 }
 void
